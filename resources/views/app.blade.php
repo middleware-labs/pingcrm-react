e6b85d2359d3bacd4a095011a6d0d53332f85b86
Hardik Choksi <hardik.choksi@middleware.io>
2024-10-15 09:58:02 +0530

    Added rum

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html class="h-full bg-gray-200">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
@@ -7,8 +8,27 @@
     @inertiaHead
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.tsx'])
+
+    <script src="https://cdnjs.middleware.io/browser/libs/0.0.1/middleware-rum.min.js" type="text/javascript"></script>
+
+    <script>
+        if (window.Middleware) {
+            Middleware.track({
+                serviceName: "pingcrm",
+                projectName: "pingcrm",
+                accountKey: "qrahdmheulnamujmetjwagwrozbmrwbpvasy",
+                target: "https://plo4e.middleware.io",
+                defaultAttributes: {
+                    "app.version": "1.0.0",
+                },
+                tracePropagationTargets: [/localhost:3000/i]
+            })
+        }
+    </script>
 </head>
+
 <body class="font-sans antialiased leading-none text-gray-800">
-@inertia
+    @inertia
 </body>
+
 </html>
